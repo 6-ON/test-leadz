4148fc713f2ade2244aa163b4e2221a373233b5c
6-ON <aminhatim6@gmail.com>
2024-02-29 19:54:53 +0100

    add missing HasLifecycleCallbacks annotation

--- leadz-api/src/Entity/Review.php
+++ leadz-api/src/Entity/Review.php
@@ -4,17 +4,13 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
 #[ApiResource]
 class Review
 {
