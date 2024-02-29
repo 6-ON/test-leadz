14ef8dffc9e6f98843d844a6774f557378bb1476
6-ON <aminhatim6@gmail.com>
2024-02-29 09:28:21 +0100

    Update theme configuration to include custom fonts

--- front-end/src/config/theme.ts
+++ front-end/src/config/theme.ts
@@ -1,3 +1,9 @@
 import { extendTheme } from '@chakra-ui/react'
-
-export const theme = extendTheme({})
+import '../../node_modules/@fontsource/open-sans';
+import '../../node_modules/@fontsource/raleway';
+export const theme = extendTheme({
+    fonts: {
+        heading: `'Open Sans', sans-serif`,
+        body: `'Raleway', sans-serif`,
+      },
+})
