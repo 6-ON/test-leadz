24ae5c8207c993d49c4a841dd0f637d7265e7c4f
6-ON <aminhatim6@gmail.com>
2024-02-29 09:30:00 +0100

    add main layout

--- front-end/src/layouts/main-layout.tsx
+++ front-end/src/layouts/main-layout.tsx
@@ -1,7 +1,20 @@
 import { Box } from '@chakra-ui/react'
+import Navbar from '@/components/partial/navbar'
+import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
+import PLHBooksPage from '@/placeholders/views/books'
 
 const MainLayout = () => {
-	return <Box></Box>
+	return (
+		<Box>
+			<Navbar />
+			<div className="container m-auto">
+				<Suspense fallback={<PLHBooksPage />}>
+					<Outlet />
+				</Suspense>
+			</div>
+		</Box>
+	)
 }
 
 export default MainLayout
