3f4800a308b46d3ee4d5f475fc31fd2b18620890
6-ON <aminhatim6@gmail.com>
2024-03-02 14:43:13 +0100

    add factories

--- leadz-api/src/DataFixtures/AppFixtures.php
+++ leadz-api/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AuthorFactory;
+use App\Factory\BookFactory;
+use App\Factory\ReviewFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +12,9 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // // $product = new Product();
-        // // $manager->persist($product);
+        AuthorFactory::createMany(40);
+        BookFactory::createMany(40*5);
+        ReviewFactory::createMany(1000);
 
-        // $manager->flush();
     }
 }
