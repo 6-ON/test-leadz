25cd09695df44f150c79d9493d2dcb9d7c57d0f2
6-ON <aminhatim6@gmail.com>
2024-03-02 20:36:14 +0100

    add reviews prop

--- front-end/src/components/partial/reviews-section.tsx
+++ front-end/src/components/partial/reviews-section.tsx
@@ -1,17 +1,20 @@
-import { Heading, SimpleGrid } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 import ReviewCard from '../ui/review-card'
-
-const ReviewsSection = () => {
+import { Review } from '@/interfaces'
+type Props = {
+	reviews: Review[]
+}
+const ReviewsSection: React.FC<Props> = ({ reviews }) => {
 	return (
-		<div className="mt-12 max-md:text-center  px-2">
+		<div className="mt-12 max-md:text-center  px-2 mb-12">
 			<Heading size="md" className="mb-6">
 				Reviews
 			</Heading>
-			<SimpleGrid minChildWidth={'xs'} spacing={5}>
-				{Array.from({ length: 10 }).map((_, i) => (
-					<ReviewCard key={i} />
+			<div className="flex flex-wrap gap-4 justify-center">
+				{reviews.map((review) => (
+					<ReviewCard key={review['@id']} review={review} />
 				))}
-			</SimpleGrid>
+			</div>
 		</div>
 	)
 }
